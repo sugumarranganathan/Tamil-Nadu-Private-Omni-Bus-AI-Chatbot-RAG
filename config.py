6f06fba06267c64db717cc8c8b4033852be1925c
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-21 19:38:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ MODEL_NAME = "all-MiniLM-L6-v2"
 
 DATA_FILE = "data/bus_services.csv"
 
-INDEX_FILE = "vector_db/watch_index.faiss"
+INDEX_FILE = "vector_db/bus_index.faiss"
 
-CHUNKS_FILE = "vector_db/chunks.pkl"
+CHUNKS_FILE = "vector_db/documents.pkl"
