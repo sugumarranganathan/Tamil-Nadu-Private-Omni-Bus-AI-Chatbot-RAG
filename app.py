93d53ba2c059882605e8af28e28ca6f35f335867
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-21 19:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,10 @@ if __name__ == "__main__":
 
     PORT = int(os.environ.get("PORT", 7860))
 
-    demo.launch(
-        server_name="0.0.0.0",
-        server_port=PORT,
-        share=False
-    )
+    demo.queue()
+
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=PORT,
+    share=False
+)
